27c4d9f87f50d3f1bbdc89c11588ce42ccf6861c
ktk8916 <ktk8916@naver.com>
2022-10-01 11:34:26 +0900

    start

---
+++ src/main/java/com/example/codetestweb/CodetestwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.codetestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetestwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetestwebApplication.class, args);
+	}
+
+}
